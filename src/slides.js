952aad6e9989edca1e705a08b6ea71bed5aaa29b
Jens Andreasson <jensandreasson77@gmail.com>
2023-11-17 22:50:15 +0100

    ✨

--- src/slides.js
+++ src/slides.js
@@ -85,17 +85,17 @@ export function run(element) {
           <div class="controls">
             <button id="prevLesson" ${
               hasPrevLesson ? '' : 'disabled'
-            }>&lsaquo;&lsaquo;</button>
+            }><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M440-240 200-480l240-240 56 56-183 184 183 184-56 56Zm264 0L464-480l240-240 56 56-183 184 183 184-56 56Z"/></svg></button>
             <button id="prevSlide" ${
               hasPrevSlide ? '' : 'disabled'
-            }>&lsaquo;</button>
-            <button id="questions">?</button>
+            }><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/></svg></button>
+            <button id="questions"><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h168q13-36 43.5-58t68.5-22q38 0 68.5 22t43.5 58h168q33 0 56.5 23.5T840-760v560q0 33-23.5 56.5T760-120H200Zm0-80h560v-560H200v560Zm80-80h280v-80H280v80Zm0-160h400v-80H280v80Zm0-160h400v-80H280v80Zm200-190q13 0 21.5-8.5T510-820q0-13-8.5-21.5T480-850q-13 0-21.5 8.5T450-820q0 13 8.5 21.5T480-790ZM200-200v-560 560Z"/></svg></button>
             <button id="nextSlide" ${
               hasNextSlide ? '' : 'disabled'
-            }>&rsaquo;</button>
+            }><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/></svg></button>
             <button id="nextLesson" ${
               hasNextLesson ? '' : 'disabled'
-            }>&rsaquo;&rsaquo;</button>
+            }><svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M383-480 200-664l56-56 240 240-240 240-56-56 183-184Zm264 0L464-664l56-56 240 240-240 240-56-56 183-184Z"/></svg></button>
           </div>
         </footer>
         <div id="questionsModal" class="modal">
